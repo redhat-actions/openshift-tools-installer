6073f147c63488b76da2522398308fbc9a990338
Tim Etchells <tetchell@redhat.com>
2020-12-30 15:57:32 -0500

    update
    Signed-off-by: Tim Etchells <tetchell@redhat.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 
 import * as ghCore from "@actions/core";
+import * as semver from "semver";
+
 import { Inputs } from './generated/inputs-outputs';
+
+const LATEST = "latest";
 
 export async function run() {
 
@@ -8,17 +12,33 @@ export async function run() {
 
     for (const input of Object.values(Inputs)) {
         const clientVersion = ghCore.getInput(input);
-        ghCore.info(`The version of ${input} to install is "${clientVersion}"`);
+        ghCore.info(`The raw version of ${input} to install is "${clientVersion}"`);
 
-        if (clientVersion === "latest" || clientVersion === "") {
-            clientsToInstall[input] = "latest";
-        }
-        else if (clientVersion != null) {
-            clientsToInstall[input] = clientVersion;
+        if (clientVersion) {
+            clientsToInstall[input] = parseVersion(clientVersion);
         }
     }
 
     console.log("The clients to install are: ", clientsToInstall);
+}
+
+function parseVersion(rawVersion: string): string {
+    let version = rawVersion;
+
+    if (version === LATEST) {
+        return LATEST;
+    }
+
+    if (rawVersion.startsWith("v")) {
+        rawVersion = rawVersion.substring(1, rawVersion.length);
+    }
+
+    const coerced = semver.coerce(rawVersion);
+    if (coerced == null) {
+        throw new Error(`Could not coerce "${rawVersion}"`);
+    }
+
+    return coerced.version;
 }
 
 run()
